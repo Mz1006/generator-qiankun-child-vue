b01a226e0d5d53a636e811bde964e2ff3f0486ca
zhangmengzhu <zmz.bajiezu.cn>
2021-06-22 12:22:13 +0800

    修改样式

--- sub-vue/src/App.vue
+++ sub-vue/src/App.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style>
-#app {
+#sub-vue {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
